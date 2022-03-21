8918ec91c25625d0535255899235d41d16d92ef7
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-03-21 11:35:58 -0300

    feat: add /orders get router

--- src/controllers/ordersControllers.js
+++ src/controllers/ordersControllers.js
@@ -28,3 +28,79 @@ export async function postOrder(req, res) {
     }
 }
 
+export async function getOrders(req, res) {
+    const { date } = req.query;
+
+    try {
+        const conditions = [];
+        const params = [];
+        let where = "";
+
+        const isThereOrder = await db.query(`SELECT * FROM orders`);
+        if (isThereOrder.rowCount === 0) return res.status(404).send([]);
+
+        if (date) {
+            const isThereOrderByDate = await db.query(`
+                    SELECT "createdAt" FROM orders WHERE "createdAt"=$1`, [date]);
+            if (isThereOrderByDate.rowCount === 0) return res.status(404).send([]);
+
+            params.push(date);
+            conditions.push(`orders."createdAt"=$${params.length}`);
+        }
+        if (params.length > 0) where += `WHERE ${conditions.join(" AND ")}`;
+
+        const result = await db.query({
+            text: `
+                SELECT 
+                    clients.*,
+                    cakes.*,
+                    orders."createdAt", orders.quantity, orders."totalPrice"
+                FROM orders
+                        JOIN clients ON clients.id=orders."clientId"
+                        JOIN cakes ON cakes.id=orders."cakeId"
+                ${where}
+            `,
+            rowMode: "array",
+        }, params);
+
+        res.status(200).send(result.rows.map(mapOrdersArrayToObject));
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+function mapOrdersArrayToObject(row) {
+    const [
+        clientId,
+        clientName,
+        clientAddress,
+        clientPhone,
+        cakeId,
+        cakeName,
+        cakePrice,
+        cakeImage,
+        cakeDescription,
+        createdAt,
+        quantity,
+        totalPrice,
+    ] = row;
+
+    return {
+        client: {
+            id: clientId,
+            name: clientName,
+            address: clientAddress,
+            phone: clientPhone,
+        },
+        cake: {
+            id: cakeId,
+            name: cakeName,
+            price: cakePrice,
+            description: cakeDescription,
+            image: cakeImage,
+        },
+        createdAt,
+        quantity,
+        totalPrice,
+    };
+}
--- src/routes/ordersRouters.js
+++ src/routes/ordersRouters.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { postOrder } from '../controllers/ordersControllers.js';
+import { getOrders, postOrder } from '../controllers/ordersControllers.js';
 import { validateSchemaMiddleware } from '../middlewares/validateSchemaMiddleware.js';
 import ordersSchema from '../schemas/ordersSchema.js';
 
 const ordersRouters = Router();
 ordersRouters.post('/order', validateSchemaMiddleware(ordersSchema), postOrder);
+ordersRouters.get('/orders', getOrders);
 
 
 export default ordersRouters;
